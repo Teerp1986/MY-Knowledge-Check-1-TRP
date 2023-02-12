c184c5043ccb49af61580ccf184b9567491c6d5b
Tonika Price <tprice312@gmail.com>
2023-02-12 09:17:16 -0500

    first commit

--- KnowledgeCheck1_Calculator/SoftwareKnowledge-Check-1Clone-TRP.cs
+++ KnowledgeCheck1_Calculator/SoftwareKnowledge-Check-1Clone-TRP.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace KnowledgeCheck1_Calculator
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
